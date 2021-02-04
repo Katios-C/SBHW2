36de1b79c9021e544b0ada8e2766298f17597a40
Katios <kate.chonkina@gmail.com>
2021-02-04 12:00:30 +0600

    Initial Commit

---
+++ HomeWork1/HomeWork1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork1App.swift
+//  HomeWork1
+//
+//  Created by Екатерина Чернова on 04.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
